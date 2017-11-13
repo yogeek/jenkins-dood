9aeb269b266e4d03b10422701d5be1341fadf328
yogeek <gdupin@gmail.com>
2017-11-13 01:16:44 +0100

    use awk if getent not installed to find docker GID

--- run.sh
+++ run.sh
@@ -6,12 +6,18 @@
 # So we pass the host docker group id in a environment variable
 # in order to put jenkins in the docker group dynamically (at run)
 # and not statically (in Dockerfile)
-DOCKER_GID=$(getent group docker | cut -d: -f3)
-# awk solution
-# awk -F\: '{print "Group " $1 " with GID=" $3}' /etc/group | grep docker
+
+# Search for docker group GID
+if [[ $(command -v getent >/dev/null 2>&1) ]]
+then
+  DOCKER_GID=$(getent group docker | cut -d: -f3)
+else
+  # getent not installed => /etc/group and awk instead
+  DOCKER_GID=$(cat /etc/group | grep docker: | awk -F\: '{print $3}')
+fi
 
 docker run --rm -it \
-    -e DOCKER_GID=${DOCKER_GID}
+    -e DOCKER_GID=${DOCKER_GID} \
 		-v /var/run/docker.sock:/var/run/docker.sock \
 		-v $(which docker):/usr/bin/docker \
 		-v jenkins-data:/var/jenkins_home  \
